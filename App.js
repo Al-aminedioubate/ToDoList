3ec9853f155cf317330adb78c933a4e8133db05e
Lamine DIOUBATE <domanilamine96@gmail.com>
2023-09-20 20:33:08 -0400

    design card des taches terminer

--- App.js
+++ App.js
@@ -2,7 +2,14 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Text, View } from "react-native";
 import { s } from "./App.style";
 import { Header } from "./components/header";
+import { CardToDo } from "./components/CardTodo/cardTodo";
 
+const TODO_LIST = [
+  { id: 1, title: "Sortir le chien", isCompleted: true },
+  { id: 2, title: "Aller chez le garagiste", isCompleted: true },
+  { id: 3, title: "Faire les courses", isCompleted: true },
+  { id: 4, title: "Appeler le veterinaire", isCompleted: true },
+];
 export default function App() {
   return (
     <>
@@ -12,7 +19,7 @@ export default function App() {
             <Header />
           </View>
           <View style={s.body}>
-            <Text>Body</Text>
+            <CardToDo todo={TODO_LIST[0]} />
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
---
+++ components/CardTodo/cardTodo.jsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity, Text, Image } from "react-native";
+import {s} from "./card.style";
+import checkImage from "../../assets/check.png";
+
+export function CardToDo({todo}){
+    return (
+        <TouchableOpacity style={s.card}>
+            <Text style={[s.txt, todo.isCompleted && {textDecorationLine:"line-through"}]}>{todo.title}</Text>
+            {todo.isCompleted && <Image style={s.img} source={checkImage} />}
+        </TouchableOpacity>
+    );
+}
