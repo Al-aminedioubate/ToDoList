45108b597efbc688c34c40ba7d0765db3ace54cc
Lamine DIOUBATE <domanilamine96@gmail.com>
2023-09-23 18:32:11 -0400

    listes des taches terminer.

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { s } from "./App.style";
 import { Header } from "./components/header";
 import { CardToDo } from "./components/CardTodo/cardTodo";
+import { useState } from "react";
 
-const TODO_LIST = [
-  { id: 1, title: "Sortir le chien", isCompleted: true },
-  { id: 2, title: "Aller chez le garagiste", isCompleted: true },
-  { id: 3, title: "Faire les courses", isCompleted: true },
-  { id: 4, title: "Appeler le veterinaire", isCompleted: true },
-];
 export default function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Aller au travail la matin", isCompleted: true },
+    { id: 2, title: "Programmer mon app", isCompleted: true },
+    { id: 3, title: "Aller a la salle de sport", isCompleted: true },
+    { id: 4, title: "Aller dormir", isCompleted: true },
+    { id: 5, title: "Aller au travail la matin", isCompleted: false },
+    { id: 6, title: "Programmer mon app", isCompleted: true },
+    { id: 7, title: "Aller a la salle de sport", isCompleted: true },
+    { id: 8, title: "Aller dormir", isCompleted: false },
+  ]);
+
+  //La fonction permettant de retourner le tableau.
+  function renderTodoList() {
+    return todoList.map((todo) => (
+      <View style={s.cardItem} key={todo.id}>
+        <CardToDo todo={todo} />
+      </View>
+    ));
+  }
+
   return (
     <>
       <SafeAreaProvider>
@@ -19,7 +34,7 @@ export default function App() {
             <Header />
           </View>
           <View style={s.body}>
-            <CardToDo todo={TODO_LIST[0]} />
+            <ScrollView>{renderTodoList()}</ScrollView>
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
--- components/CardTodo/cardTodo.jsx
+++ components/CardTodo/cardTodo.jsx
@@ -5,7 +5,7 @@ import checkImage from "../../assets/check.png";
 export function CardToDo({todo}){
     return (
         <TouchableOpacity style={s.card}>
-            <Text style={[s.txt, todo.isCompleted && {textDecorationLine:"line-through"}]}>{todo.title}</Text>
+            {<Text style={[s.txt, todo.isCompleted && {textDecorationLine:"none"}]}>{todo.title}</Text>}
             {todo.isCompleted && <Image style={s.img} source={checkImage} />}
         </TouchableOpacity>
     );
